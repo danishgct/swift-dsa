c3164e594cba6bd77eccdd8090f183d6b19a579d
Danish Phiroz <danishgct@gmail.com>
2025-05-30 17:54:21 -0400

    Initial Commit

---
+++ DSA/DSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSAApp.swift
+//  DSA
+//
+//  Created by Danish Phiroz on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct DSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
